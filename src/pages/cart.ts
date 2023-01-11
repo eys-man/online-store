5d70d5fc170f56656dd4ae253d183512ffcd76c6
eys-man <kapuec2010@gmail.com>
2023-01-11 23:53:22 +0300

    feat: add pagination to shoppin-cart

--- src/pages/cart.ts
+++ src/pages/cart.ts
@@ -3,36 +3,130 @@ import Page from '../templates/page';
 import ShoppingCard from '../templates/shopping-card/shopping-card';
 
 class CartPage extends Page {
+    numPages: number; // полное число страниц
+    curPage: number; // текущая страницп
+    itemsPerPage: number; // макс. число товаров на странице
     constructor(id: string) {
         super(id);
+        this.curPage = 1; // страница по умолчанию
+        this.itemsPerPage = 5; // по умолчанию
+        this.numPages = Math.ceil(Data.selectedItems.size / this.itemsPerPage);
+        // alert('numPages ' + this.numPages + ', curPage ' + this.curPage + ', itemsPerPage ' + this.itemsPerPage);
     }
 
     render() {
-        // this.container.className = 'cart';
-        const cart = document.createElement('div');
-        cart.className = 'shopping-cart';
+        this.container.innerHTML = '';
+        if (Data.selectedItems.size != 0) {
+            this.numPages = Math.ceil(Data.selectedItems.size / this.itemsPerPage);
+            if (this.curPage > this.numPages) this.curPage = this.numPages;
 
-        const cardList = document.createElement('div');
-        cardList.className = 'card-list';
-        cart.append(cardList);
+            const cart = document.createElement('div');
+            cart.className = 'shopping-cart';
 
-        const cardListHeader = document.createElement('div');
-        cardListHeader.className = 'card-list-header';
-        cardList.append(cardListHeader);
+            const cardList = document.createElement('div');
+            cardList.className = 'card-list';
+            cart.append(cardList);
 
-        Data.selectedItems.forEach((x) => {
-            const card = new ShoppingCard('div', 'shopping-card', x.id);
+            const cardListHeader = document.createElement('div');
+            cardListHeader.className = 'card-list-header';
 
-            cardList.append(card.render());
-        });
-        cart.append(cardList);
+            const limit = document.createElement('div');
+            limit.className = 'limit';
 
-        const summary = document.createElement('div');
-        summary.className = 'summary';
-        cart.append(summary);
+            // число товаров на странице
+            const limitP = document.createElement('p');
+            limitP.innerText = 'Items per page (3 ↔ 10)';
+            limit.append(limitP);
 
-        this.container.append(cart);
+            const limitInput = document.createElement('input');
+            limitInput.type = 'number';
 
+            limitInput.value = `${this.itemsPerPage}`;
+            limitInput.min = '3';
+            limitInput.max = '10';
+            limitInput.step = '1';
+
+            limit.append(limitInput);
+            // TODO:
+
+            cardListHeader.append(limit);
+
+            // номер отображаемой страницы
+            const page = document.createElement('div');
+            page.className = 'page';
+
+            const pageP = document.createElement('p');
+            pageP.innerText = 'Page';
+            page.append(pageP);
+
+            const pageInput = document.createElement('input');
+            pageInput.type = 'number';
+
+            pageInput.type = 'number';
+
+            pageInput.value = `${this.curPage}`;
+            pageInput.min = '1';
+            pageInput.max = `${this.numPages}`;
+            pageInput.step = '1';
+
+            page.append(pageInput);
+
+            cardListHeader.append(page);
+
+            cardList.append(cardListHeader);
+
+            // вывод списка товаров в корзине
+            let serialNumber: number = 1; // текущий номер товара (пишется спереди)
+            const beginNumber = this.itemsPerPage * (this.curPage - 1) + 1; // с этого номера начнется отображение
+            Data.selectedItems.forEach((x) => {
+                if (serialNumber >= beginNumber && serialNumber < beginNumber + this.itemsPerPage) {
+                    const card = new ShoppingCard('div', 'shopping-card', x, serialNumber, this);
+                    cardList.append(card.render());
+                }
+                serialNumber += 1;
+            });
+            cart.append(cardList);
+
+            const summary = document.createElement('div');
+            summary.className = 'summary';
+            cart.append(summary);
+
+            limitInput.addEventListener('input', () => {
+                if (Number(limitInput.value) > 10) {
+                    console.log('много');
+                    this.itemsPerPage = 10;
+                    limitInput.value = `${this.itemsPerPage}`;
+                } else if (Number(limitInput.value) < 3) {
+                    console.log('мало');
+                    this.itemsPerPage = 3;
+                    limitInput.value = `${this.itemsPerPage}`;
+                } else {
+                    this.itemsPerPage = Number(limitInput.value);
+                }
+                this.render();
+            });
+
+            pageInput.addEventListener('input', () => {
+                if (Number(pageInput.value) > this.numPages) {
+                    console.log('много');
+                    this.curPage = this.numPages;
+                    pageInput.value = `${this.curPage}`;
+                } else if (Number(pageInput.value) < 1) {
+                    console.log('мало');
+                    pageInput.value = '1';
+                    this.curPage = 1;
+                } else {
+                    this.curPage = Number(pageInput.value);
+                }
+                this.render();
+            });
+
+            this.container.append(cart);
+        } else {
+            const empty = document.createElement('p');
+            empty.innerText = 'Cart is empty';
+            this.container.append(empty);
+        }
         return this.container;
     }
 }
--- src/templates/shopping-card/shopping-card.ts
+++ src/templates/shopping-card/shopping-card.ts
@@ -1,18 +1,28 @@
+import CartPage from '../../pages/cart';
 import Component from '../components';
-import Data, { Product } from '../data';
+import Data, { Product, Item } from '../data';
+import Header from '../header/header';
 // import Header from '../header/header';
 import './shopping-card.css';
 
 class ShoppingCard extends Component {
+    parent: CartPage;
+    serialNumber: number;
     private id: number; // id продукта
-    item!: Product;
-    constructor(tagName: string, className: string, id: number) {
+    private quantity: number;
+    item!: Product; // в массиве всех товаров
+    cartItem!: Item; // элемент множества 'товары в корзине'
+    constructor(tagName: string, className: string, x: Item, num: number, parent: CartPage) {
         super(tagName, className);
-        this.id = id;
+        this.parent = parent;
+        this.serialNumber = num;
+        this.id = x.id;
+        this.quantity = x.quantity;
         let i: number = 0;
         while (i < Data.products.length - 1) {
-            if (id === Data.products[i].id) {
+            if (x.id === Data.products[i].id) {
                 this.item = Data.products[i];
+                this.cartItem = x;
                 break; // нашли товар
             }
             i += 1;
@@ -20,8 +30,10 @@ class ShoppingCard extends Component {
     }
 
     render() {
-        // ---------------- карточка товара ---------------------
-        // this.container.className = 'shopping-card';
+        const serialNumber = document.createElement('div');
+        serialNumber.className = 'serial-number';
+        serialNumber.innerText = `${this.serialNumber}`;
+        this.container.append(serialNumber);
 
         const img = document.createElement('img');
         img.className = 'shopping-card-img';
@@ -57,8 +69,74 @@ class ShoppingCard extends Component {
         const controls = document.createElement('div');
         controls.className = 'shopping-card-controls';
 
+        // кнопка уменьшить кол-во
+        const decrease = document.createElement('button');
+        decrease.className = 'decrease';
+        decrease.innerText = '–';
+        controls.append(decrease);
+
+        //-------
+        const itemInfo = document.createElement('div');
+        itemInfo.className = 'item-info';
+
+        const stock = document.createElement('p');
+        stock.className = 'in-stock';
+        stock.innerText = `in stock: ${this.item.stock}`;
+        itemInfo.append(stock);
+
+        const inCart = document.createElement('p');
+        inCart.className = 'in-cart';
+        inCart.innerText = `${this.quantity}`;
+        itemInfo.append(inCart);
+
+        const price = document.createElement('p'); // цена за несколько штук этого наименования
+        price.className = 'items-price';
+        price.innerText = `€${this.item.price * this.quantity}`;
+        itemInfo.append(price);
+
+        controls.append(itemInfo);
+
+        // кнопка добавить кол-во
+        const increase = document.createElement('button');
+        increase.className = 'increase';
+        increase.innerText = '+';
+        controls.append(increase);
+
         this.container.append(controls);
 
+        increase.addEventListener('click', () => {
+            if (this.quantity < this.item.stock) {
+                this.quantity += 1; // в классе
+                this.cartItem.quantity += 1; // во множестве (по ссылке)
+
+                inCart.innerText = `${this.quantity}`;
+                price.innerText = `€${this.item.price * this.quantity}`;
+
+                Header.update();
+
+                // TODO: записать в localStorage
+            }
+        });
+
+        decrease.addEventListener('click', () => {
+            if (this.quantity > 1) {
+                this.quantity -= 1; // в классе
+                this.cartItem.quantity -= 1; // во множестве (по ссылке)
+
+                inCart.innerText = `${this.quantity}`;
+                price.innerText = `€${this.item.price * this.quantity}`;
+
+                Header.update();
+            } else {
+                // удалить из корзины элемент
+                Data.selectedItems.delete(this.cartItem);
+                Header.update();
+
+                this.container.remove();
+                this.parent.render();
+            }
+            // TODO: записать в localStorage
+        });
         return this.container;
     }
 }
